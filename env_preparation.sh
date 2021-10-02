adf51ef5b2fe83b33dad0dc384d814a60ba96346
Soroush Hashemi Far <soroush.hashemi76@gmail.com>
2021-10-02 11:24:54 +0330

    Update env_preparation.sh

--- env_preparation.sh
+++ env_preparation.sh
@@ -1,4 +1,4 @@
-env_name="*_env"
+env_name="SOMENAME_env"
 py_version=3.7
 tf_version=2.4.1
 
@@ -10,7 +10,13 @@ conda install -y -c anaconda python=$py_version
 conda install -y pip
 conda install -y -c conda-forge jupyterlab
 
-# required packages
+# TF and Torch packages
 conda install pytorch torchvision torchaudio -c pytorch -c conda-forge
 conda install -y tensorflow-gpu=$tf_version -c anaconda
+
+# Computer Vision/Graphics
 pip install opencv-python
+pip install imageio
+
+# Production level
+pip install tf2onnx onnx onnxruntime
